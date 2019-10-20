3c2f895f083c0d7dc39b32d61783ab8dbc8aa5c0
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-10-20 11:06:44 +0200

    authorization added to login route with token being sent as a header from header

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,7 +6,14 @@ import { AuthData } from "./auth-data.model";
   providedIn: "root"
 })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
+
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
@@ -19,9 +26,10 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
-      .post("http://localhost:3000/api/user/login", authData)
+      .post<{ token: string }>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
