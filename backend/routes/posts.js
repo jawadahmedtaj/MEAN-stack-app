7dee50c4f49df42ba61e2236dd703defd7c54dda
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-10-21 11:29:53 +0200

    authorized user can only edit and delete now

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -71,9 +71,14 @@ router.put(
       content: req.body.content,
       imagePath: imagePath
     });
-    console.log(post);
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-      res.status(200).json({ message: "Update successful!" });
+    // console.log(post);
+    Post.updateOne(
+      { _id: req.params.id, creator: req.userData.userId },
+      post
+    ).then(result => {
+      if (result.nModified > 0)
+        res.status(200).json({ message: "Update successful!" });
+      else res.status(401).json({ message: "Not Authorized" });
     });
   }
 );
@@ -111,10 +116,13 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    result => {
+      if (result.n > 0)
+        res.status(200).json({ message: "Deleted successfully!" });
+      else res.status(401).json({ message: "Not Authorized" });
+    }
+  );
 });
 
 module.exports = router;
