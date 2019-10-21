e49045a8672be29499065e8b951378172e386647
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-10-21 11:55:12 +0200

    front end now reacts to creator

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -69,7 +69,8 @@ router.put(
       _id: req.body.id,
       title: req.body.title,
       content: req.body.content,
-      imagePath: imagePath
+      imagePath: imagePath,
+      creator: req.userData.userId
     });
     // console.log(post);
     Post.updateOne(
