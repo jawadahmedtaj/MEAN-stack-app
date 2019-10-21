eb0028968b761a617cc76b9cbb87e4e721bf3705
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-10-21 17:58:43 +0200

    returning errors from the server

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -19,7 +19,9 @@ router.post("/signup", (req, res, next) => {
       });
     })
     .catch(err => {
-      res.status(500).json({ error: err });
+      res.status(500).json({
+        message: "Invalid authentication credentials!"
+      });
     });
 });
 
@@ -37,7 +39,7 @@ router.post("/login", (req, res, next) => {
       if (!result) {
         return res
           .status(401)
-          .json({ message: "Login failed, incorrect password" });
+          .json({ message: "Login failed, please check password/email" });
       }
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id },
@@ -53,7 +55,7 @@ router.post("/login", (req, res, next) => {
     .catch(err => {
       return res
         .status(401)
-        .json({ message: "Login failed, incorrect password" });
+        .json({ message: "Invalid authentication credentials!" });
     });
 });
 
